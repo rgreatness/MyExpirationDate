481c45adcf56b75b8d8dd2d7765d4e8ec305b074
karlyRip <126495304+karlyRip@users.noreply.github.com>
2025-12-05 22:56:00 -0500

    Fetch and display food bank locations closest to the user's current location.  Added marker window.

--- app/src/main/java/com/example/myexpirationdate/screens/MapScreen.kt
+++ app/src/main/java/com/example/myexpirationdate/screens/MapScreen.kt
@@ -5,13 +5,30 @@ import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.myexpirationdate.viewmodels.MapVM
 import com.google.android.gms.location.LocationServices
@@ -19,7 +36,14 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.example.myexpirationdate.TAG
-
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.libraries.places.api.Places
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindow
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberMarkerState
+import com.example.myexpirationdate.R
+import com.google.android.libraries.places.api.model.Place
 
 
 @Composable
@@ -28,7 +52,9 @@ fun MapScreen(mapVM: MapVM, modifier: Modifier = Modifier) {
     val cameraPositionState = rememberCameraPositionState()
     val context = LocalContext.current
     val userLocation by mapVM.userLocation
+    val foodBankLocations by mapVM.foodBankLocations
     val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context)}
+    val placesClient = remember{ Places.createClient(context) }
 
     //handle permission requests for fine location
     val permissionLauncher = rememberLauncherForActivityResult(
@@ -63,37 +89,85 @@ fun MapScreen(mapVM: MapVM, modifier: Modifier = Modifier) {
     ){
         userLocation?.let {
             cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 10f)
+
+            //Fetch nearby food bank location
+            mapVM.fetchFoodBankLocations(context, placesClient)
+
+            //loop through locations and make a marker for each one
+            for (location in foodBankLocations){
+                if (location.name != "Placeholder"){
+                    println(location.name)
+
+//                    Marker(
+//                        state = rememberMarkerState(position = location.latLng),
+//                        draggable = false,
+//                        title = location.name,
+//                        snippet = location.address + location.openingHours,
+//                        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)
+//                    )
+
+                    MarkerInfoWindow(
+                        state = rememberMarkerState(position = location.latLng),
+                        title = location.name,
+                        icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE),
+                        onInfoWindowClick = { it ->
+                            it.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+                        },
+                        onInfoWindowClose = {
+                            it.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                        }
+                    ){
+                        FoodBankWindow(location)
+                    }
+                }
+            }
         }
     }
-
-//    Column(
-//        modifier = modifier.fillMaxSize(),//.padding(16.dp),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally
-//    ){
-
-
-//        val uri = Uri.parse("https://www.google.com/maps/search/food+bank/@${latitude},${longitude},188000m") //""https://www.google.com/maps/search/food+bank/@${latitude},${longitude},188000m/?api=1")
-//
-//        Intent(Intent.ACTION_VIEW, uri).also {
-//            context.startActivity(it)
-//        }
-
-//        Text("Donate or volunteer at a food bank near you!")
-//
-//        Spacer(modifier = Modifier.height(20.dp))
-//
-//        Button(
-//            onClick = {
-//                val uri = Uri.parse("https://www.google.com/maps/search/food+bank/@41.1630423,-80.229205,34143m")
-//
-//                Intent(Intent.ACTION_VIEW, uri).also {
-//                    context.startActivity(it)
-//                }
-//            }
-//        ) {
-//            Text("Find Food Banks")
-//        }
-
-//    }
 }
+
+
+@Composable
+fun FoodBankWindow(loc: Place, modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.onPrimary,
+                shape = RoundedCornerShape(35.dp, 35.dp, 35.dp, 35.dp)
+            )
+    ){
+        Column(modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(
+                text = loc.name!!,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 10.dp),
+                style = MaterialTheme.typography.headlineMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            HorizontalDivider()
+
+            Text(
+                text = loc.address!!,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(top = 10.dp),
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            for (text in loc.openingHours!!.weekdayText){
+                Text(
+                    text = text,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(top = 10.dp),
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+
+        }
+    }
+}
+
+
--- app/src/main/java/com/example/myexpirationdate/viewmodels/MapVM.kt
+++ app/src/main/java/com/example/myexpirationdate/viewmodels/MapVM.kt
@@ -1,5 +1,7 @@
 package com.example.myexpirationdate.viewmodels
 
+import android.Manifest
+import android.R
 import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
@@ -9,8 +11,15 @@ import androidx.lifecycle.ViewModel
 import com.example.myexpirationdate.TAG
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.CircularBounds
+import com.google.android.libraries.places.api.model.LocationRestriction
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
 import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.api.net.SearchByTextRequest
 import com.google.android.libraries.places.api.net.SearchNearbyRequest
+import java.util.Arrays
 
 class MapVM: ViewModel() {
 
@@ -19,16 +28,45 @@ class MapVM: ViewModel() {
     val userLocation = _userLocation
 
     //save food bank locations as latitude and longitude
-    private val _foodBankLocations = listOf(mutableStateOf<LatLng?>(null))
+    val place: Place = Place.builder().setName("Placeholder").build()
+    private var _foodBankLocations = mutableStateOf(List<Place>(
+        20,
+        init = { num -> place },
+    ))
     val foodBankLocations = _foodBankLocations
 
-    fun fetchFoodBankLocations(context: Context, placesClient: PlacesClient){
-//        val request = SearchNearbyRequest.builder()
-//            .
-//            .setMaxResultCount(50)
-//            .build()
 
-        //placesClient.searchNearby()
+
+    fun fetchFoodBankLocations(context: Context, placesClient: PlacesClient){
+
+        val placeFields: List<Place.Field> = listOf<Place.Field>(
+            Place.Field.LAT_LNG,
+            Place.Field.ID,
+            Place.Field.OPENING_HOURS,
+            Place.Field.NAME,
+            Place.Field.ADDRESS
+        )
+
+        val swLat = userLocation.value!!.latitude - 0.8
+        val swLon = userLocation.value!!.longitude - 0.8
+        val swBound = LatLng(swLat, swLon)
+
+        val neLat = userLocation.value!!.latitude + 0.8
+        val neLon = userLocation.value!!.longitude + 0.8
+        val neBound = LatLng(neLat, neLon)
+
+        val bounds = RectangularBounds.newInstance(swBound, neBound)
+        val request = SearchByTextRequest.builder("food bank", placeFields)
+            .setMaxResultCount(40)
+            .setLocationRestriction(bounds)
+            .build()
+
+        placesClient.searchByText(request)
+            .addOnSuccessListener { response ->
+                val places: List<Place> = response.places
+                _foodBankLocations.value = places
+                Log.e(TAG, "Places worked.")
+            }
 
     }
 
@@ -37,7 +75,7 @@ class MapVM: ViewModel() {
     // fetch user location and update state
     fun fetchUserLocation(context: Context, fusedLocationClient: FusedLocationProviderClient){
         // check if location permission is granted
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             try{
                 // fetch last known location
                 fusedLocationClient.lastLocation.addOnSuccessListener { location ->
